32ee3766ceb44ac55234a4bdac4b1bfb606e521c
Tarn W. Burton <twburton@gmail.com>
2023-04-05 10:15:47 -0400

    Update sequence declarations

--- code/sequence.lisp
+++ code/sequence.lisp
@@ -21,6 +21,10 @@
 (defmethod make-clause ((type (eql :each-elt)) &rest initargs)
   (apply #'make-instance 'sequence-clause :var-spec initargs))
 
+(defmethod declaration-targets ((clause sequence-clause))
+  (unless (update clause)
+    (bindings-from-d-var-spec (var-spec clause))))
+
 (defmethod wrap-outer-form ((clause sequence-clause) form)
   `(let (,(index clause)
          ,@(assemble-in-order clause
@@ -28,6 +32,10 @@
                                 :start ((,(next-index clause) ,(start clause)))
                                 :by ((,(by-var clause) ,(by-form clause)))
                                 :end ((,(end-var clause) ,(end clause))))))
+     (when (< ,(next-index clause) ,(if (end clause)
+                                        (end-var clause)
+                                        `(length ,(seq-var clause))))
+       (setq ,(index clause) ,(next-index clause)))
      ,form))
 
 (defmethod wrap-inner-form ((clause sequence-clause) form)
@@ -35,7 +43,9 @@
       `(symbol-macrolet ,(bindings-from-d-var-spec (var-spec clause)
                                                    `(elt ,(seq-var clause) ,(index clause)))
          ,form)
-      `(let ,(bindings-from-d-var-spec (var-spec clause))
+      `(let ,(bindings-from-d-var-spec (var-spec clause)
+                                       `(when ,(index clause)
+                                          (elt ,(seq-var clause) ,(index clause))))
          ,.(apply #'declarations
                   (bindings-from-d-var-spec (var-spec clause)))
          ,form)))
@@ -45,13 +55,6 @@
                                          (end-var clause)
                                          `(length ,(seq-var clause))))
       (hoop-finish))))
-
-(defmethod initial-late-forms ((clause sequence-clause))
-  `((setq ,(index clause) ,(next-index clause)
-          ,.(unless (update clause)
-              (apply #'nconc
-                     (bindings-from-d-var-spec (var-spec clause)
-                                               `(elt ,(seq-var clause) ,(index clause))))))))
 
 (defmethod next-early-forms ((clause sequence-clause))
   `((incf ,(next-index clause) ,(by-var clause))
